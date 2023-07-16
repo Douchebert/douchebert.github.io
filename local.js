bf0805ac6ecf567e0f6c024d05eb5cc48695e12c
Douchebert <robert@cederlunds.nu>
2023-07-17 01:08:39 +0200

    33

--- local.js
+++ local.js
@@ -1,12 +1,20 @@
-// Fetch the YAML file
+// Fetch the first YAML file
 fetch('/Policies/policies_l_english.yml')
     .then(response => response.text())
     .then(yamlText => {
         // Parse the YAML text into a JavaScript object
-        var data = jsyaml.load(yamlText);
+        var data1 = jsyaml.load(yamlText);
 
-        // Now you can use the data object to look up replacements
-        window.replacements = data;
+        // Fetch the second YAML file
+        return fetch('/Policies/powers_and_ideas_l_english.yml');
+    })
+    .then(response => response.text())
+    .then(yamlText => {
+        // Parse the YAML text into a JavaScript object
+        var data2 = jsyaml.load(yamlText);
+
+        // Combine the data from the two files
+        window.replacements = Object.assign({}, data1, data2);
     })
     .catch(error => console.error('Error:', error));
 
@@ -16,7 +24,7 @@ function replaceWords(text) {
   text = text.replace(/\bDIP\b/g, "Diplomatic");
   text = text.replace(/\bMIL\b/g, "Military");
 
-  // Replace other strings based on the data from the YAML file
+  // Replace other strings based on the data from the YAML files
   for (var key in window.replacements) {
     var value = window.replacements[key];
     var regex = new RegExp('\\b' + key + '\\b', 'g');
