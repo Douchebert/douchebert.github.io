8952d5c754d1f857fa7dbefd35ca47954066ab9f
Douchebert <robert@cederlunds.nu>
2023-07-17 00:43:37 +0200

    big regex  rework

--- local.js
+++ local.js
@@ -1,42 +1,44 @@
 // Standardized regex pattern for replacements
-var replacePattern = /([a-zA-Z_]+):0\s+"([^"]+)"|([a-zA-Z_]+):0\s+([^"]+)(?=\s*:0)/gi;
-
+var replacePattern = /([a-zA-Z_]+):0\s+"([^"]+)"/gi;
 
 // Replace specific case-sensitive words with their replacements
 function replaceWords(text) {
   text = text.replace(/\bADM\b/g, "Administrative");
   text = text.replace(/\bDIP\b/g, "Diplomatic");
   text = text.replace(/\bMIL\b/g, "Military");
-  text = text.replace(replacePattern, "$2");
   return text;
 }
 
-// Function to load and process the policies file
-function loadPoliciesFile() {
-  var policiesUrl = 'Policies/policies_l_english.yml'; // Replace with the correct URL
-  fetch(policiesUrl)
-    .then(response => response.text())
-    .then(text => {
-      var processedText = replaceWords(text);
-      console.log(processedText);
-    })
-    .catch(error => console.error('Error:', error));
+// Function to process the idea group details and perform replacements
+function processIdeaGroupDetails(details) {
+  // Replace specific case-sensitive words with their replacements
+  details = replaceWords(details);
+
+  // Apply additional replacements or transformations to the details as needed
+
+  return details;
 }
 
-// Function to load and process the powers and ideas file
-function loadPowersAndIdeasFile() {
-  var powersAndIdeasUrl = 'Policies/powers_and_ideas_l_english.yml'; // Replace with the correct URL
-  fetch(powersAndIdeasUrl)
-    .then(response => response.text())
-    .then(text => {
-      var processedText = replaceWords(text);
-      console.log(processedText);
-    })
-    .catch(error => console.error('Error:', error));
+// Function to handle the onchange event of the idea group select
+function displayIdeaGroupDetails() {
+  var select = document.getElementById('idea-group-select');
+  var ideaGroupName = select.value;
+
+  // Fetch the idea group details or retrieve them from your data source
+  // ...
+
+  // Simulating the fetched details for demonstration purposes
+  var fetchedDetails = ideaGroupName + ":0 \"" + ideaGroupName + " Ideas\"";
+
+  // Process the fetched details and perform replacements
+  var processedDetails = processIdeaGroupDetails(fetchedDetails);
+
+  // Display the processed details
+  document.getElementById('idea-group-details').textContent = processedDetails;
 }
 
-// Call the function to load and process the policies file
-loadPoliciesFile();
+// Call the function to load the idea group select options and set up the onchange event
+loadIdeaGroupSelect();
 
-// Call the function to load and process the powers and ideas file
-loadPowersAndIdeasFile();
+// Call the function to display the idea group details initially
+displayIdeaGroupDetails();
