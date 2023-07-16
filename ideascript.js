8b24de461513d0a565c631afe555a29f976a6a7a
Douchebert <robert@cederlunds.nu>
2023-07-16 04:22:30 +0200

    moved to js

---
+++ ideascript.js
@@ -0,0 +1,69 @@
+// URL of the ideas file
+var url = 'https://douchebert.github.io/Ideas/00_basic_ideas.txt'; 
+
+// Fetch the file
+fetch(url)
+    .then(response => response.text())
+    .then(text => {
+        // Split the text into lines
+        var lines = text.split('\n');
+
+        // Filter the lines to get the idea group names
+        var ideaGroups = lines.filter(line => line.includes('_ideas = {')).map(line => line.replace(' = {', ''));
+
+        // Get the select element
+        var select = document.getElementById('idea-group-select');
+
+        // Add an option for each idea group
+        for (var i = 0; i < ideaGroups.length; i++) {
+            var option = document.createElement('option');
+            option.value = ideaGroups[i];
+            option.text = ideaGroups[i];
+            select.appendChild(option);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+
+function displayIdeaGroupDetails(ideaGroupName) {
+    // Fetch the file again
+    fetch(url)
+        .then(response => response.text())
+        .then(text => {
+            // Find the start of the selected idea group's details
+            var start = text.indexOf(ideaGroupName + ' = {');
+
+            // Count the number of opening and closing curly braces
+            var openingBraceCount = 0;
+            var closingBraceCount = 0;
+            var i = start;
+
+            while (i < text.length) {
+                if (text[i] === '{') {
+                    openingBraceCount++;
+                } else if (text[i] === '}') {
+                    closingBraceCount++;
+                }
+
+                // Check if the idea group's details have ended
+                if (openingBraceCount > 0 && openingBraceCount === closingBraceCount) {
+                    break;
+                }
+
+                i++;
+            }
+
+            // Extract the idea group's details
+            var details = text.slice(start, i + 1);
+
+            // Remove the "ai_will_do" part
+            details = removeAIWillDo(details);
+
+            // Display the details
+            document.getElementById('idea-group-details').textContent = details;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function removeAIWillDo(details) {
+    return details.replace(/ai_will_do[\s\S]*/, '');
+}
