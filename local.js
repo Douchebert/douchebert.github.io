18b38229496b3f057ea104a416749dc09d46fff1
Douchebert <robert@cederlunds.nu>
2023-07-17 12:47:00 +0200

    45

--- local.js
+++ local.js
@@ -32,13 +32,14 @@ fetch('local.txt')
             // Split the line into the key and value
             var parts = line.split(':');
             if (parts.length === 2) {
-                var key = parts[0].trim();  // Remove spaces
-                var value = parts[1].trim();  // Remove spaces
-                
-                // Extract everything inside the quotes to get the true value
-                var trueValue = value.match(/"([^"]+)"/);
-                if (trueValue) {
-                    replacements[key] = trueValue[1];
+                var key = parts[0].trim();
+                var value = parts[1].trim();
+
+                // Extract the string between quotes
+                var match = value.match(/"(.+)"/);
+                if (match && match[1]) {
+                    // Store the replacement in the global object
+                    replacements[key] = match[1];
                 }
             }
         }
